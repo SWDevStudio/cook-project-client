4785a5c35a67b103dbb54ab2772c1d3651f55897
Kotaro <swdevstudio@gmail.com>
2022-12-04 13:42:11 +0300

    Добавил новую страницу с шаблоном

---
+++ src/layout/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    default
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,27 @@
 import * as VueRouter  from 'vue-router'
 import Main from '@/pages/Main.vue'
 import Empty from '@/layout/Empty.vue'
+import Default from '@/layout/Default.vue'
 
 const routes: VueRouter.RouteRecordRaw[] = [
     {
         path: '/',
         component: Empty,
         children: [
-            { path: '/', component: Main}
+            { path: '/', component: Main }
+        ]
+    },
+    {
+        path: '/',
+        component: Default,
+        children: [
+            { path: 'personal-area', component: () => import('../pages/PersonalArea.vue') }
         ]
     }
 ]
 
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
+    history: VueRouter.createWebHistory(),
     routes
 })
 
