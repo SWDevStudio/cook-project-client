746fcd90518379d7314ce504a9968245b3cf22a8
Kotaro <swdevstudio@gmail.com>
2022-12-04 15:27:51 +0300

    Снес prime

--- src/layout/Default.vue
+++ src/layout/Default.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    default
-    <RouterView />
+  <div class="flex-row">
+    <div>
+      <Menu
+          :model="items"
+      >
+
+      </Menu>
+    </div>
+    <div>
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import Menu from "primevue/menu";
+
+const items = [
+  {
+    label: 'Options',
+    items: [{label: 'New', icon: 'pi pi-fw pi-plus', command:() => {} },
+      {label: 'Delete', icon: 'pi pi-fw pi-trash', url: 'https://www.primetek.com.tr'}]
+  },
+  {
+    label: 'Account',
+    items: [{label: 'Options', icon: 'pi pi-fw pi-cog', to: '/options'},
+      {label: 'Sign Out', icon: 'pi pi-fw pi-power-off', to: '/logout'} ]
+  }
+];
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,8 @@ import { createApp } from 'vue'
 import router from "./router";
 import App from './App.vue'
 
-import PrimeVue from 'primevue/config';
-import 'primeflex/primeflex.css'
-import 'primevue/resources/themes/md-dark-indigo/theme.css'
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css';
-
-import InputText from "primevue/inputtext";
-import Checkbox from "primevue/checkbox";
-import Button from "primevue/button";
-
 const app = createApp(App)
 app.use(router)
-app.use(PrimeVue)
 
-app.component('InputText', InputText)
-app.component('Checkbox', Checkbox)
-app.component('Button', Button)
 
 app.mount('#app')
